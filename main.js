d8f22c241fc4b54c033a3a3392cf10be19f8a9c6
DaryaSerikova <dvserikova@gmail.com>
2021-12-21 00:51:49 +0700

    with hints

--- main.js
+++ main.js
@@ -8,9 +8,18 @@ var riddle = {
             const message =  (guessAnswer.toLowerCase() === this.correctAnswer) ? "Правильный ответ" : "Неправильный ответ";
             alert(message);
             console.log(message); //the repeating in console.log for autotest
-            // if (this.tries === 1) {
 
-            // }
+            console.log(`Текущая попытка: ${this.tries}`);
+            
+            if (this.tries <= 3 && message === "Неправильный ответ") {//2-3+1 //2-2+1
+                let indexHint = this.hints.length - this.tries + 1 // 2-2 //2-1
+                // console.log(`indexHint ${indexHint}`);
+
+                if (indexHint < 2) {
+                    alert(`Подсказка: ${this.hints[indexHint]}`);
+                    // console.log(`Hint: ${this.hints[indexHint]}`);
+                }
+            }
             this.tries--;
         } else {
             alert("Игра окончена");
@@ -34,7 +43,7 @@ function check() {
     if (guessedAnswer) {
         
         riddle.checkAnswer(guessedAnswer);
-        console.log(riddle.tries);
+        // console.log(riddle.tries);
 
     }
 }
